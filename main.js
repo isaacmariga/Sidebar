21ca39c10411e3318ef0791082273389f752e849
isaacmariga <isaac.wang'ombe@student.moringaschool.com>
2022-02-17 09:44:02 +0300

    Adding functionality to the ham and x

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
 let ham = document.querySelector(".ham")
 let cross = document.querySelector(".cross")
 let sbar = document.querySelector(".sidebar")
+let sStyle = getComputedStyle(sbar)
+
+
+cross.addEventListener("click", event => {
+    if (sStyle.display.includes("block")) {
+        sbar.style.display = "none"
+    } else { sbar.style.display = "block" }
+
+})
+ham.addEventListener("click", event => {
+    if (sStyle.display.includes("block")) {
+        sbar.style.display = "none"
+    } else { sbar.style.display = "block" }
+
+})
