5b1d64ce729592269965814f3a2cbb3e3b772120
isaacmariga <isaac.wang'ombe@student.moringaschool.com>
2022-02-17 10:25:18 +0300

    Adding the toggle functionality with classList

--- main.js
+++ main.js
@@ -4,15 +4,27 @@ let sbar = document.querySelector(".sidebar")
 let sStyle = getComputedStyle(sbar)
 
 
-cross.addEventListener("click", event => {
-    if (sStyle.display.includes("block")) {
-        sbar.style.display = "none"
-    } else { sbar.style.display = "block" }
+// cross.addEventListener("click", event => {
+//     if (sStyle.display.includes("block")) {
+//         sbar.style.display = "none"
+//     } else { sbar.style.display = "block" }
 
+// })
+// ham.addEventListener("click", event => {
+//     if (sStyle.display.includes("block")) {
+//         sbar.style.display = "none"
+//     } else { sbar.style.display = "block" }
+
+// })
+
+ham.addEventListener("click", function() {
+    // console.log(sbar.classList);
+    //     if (sbar.classList.contains("sidebar-show")) {
+    //         sbar.classList.remove("sidebar-show")
+    //     } else { sbar.classList.add("sidebar-show") }
+    // })
+    sbar.classList.toggle("sidebar-show");
 })
-ham.addEventListener("click", event => {
-    if (sStyle.display.includes("block")) {
-        sbar.style.display = "none"
-    } else { sbar.style.display = "block" }
-
+cross.addEventListener("click", function() {
+    sbar.classList.remove("sidebar-show");
 })
